1ffe35fa67c1b2503a71aa5ebbd69917b985a65e
Saturnxs <bmthomas.code@gmail.com>
2024-03-20 14:44:36 -0600

    Added floating animation

--- PASO4/src/components/Cover.tsx
+++ PASO4/src/components/Cover.tsx
@@ -28,8 +28,8 @@ const Cover: React.FC = () => {
             <div className="row justify-content-center pt-5">
                 <div className="col-sm-8 col-12 text-center">
                     <h3 className="text-white"><Trans i18nKey="cover.scroll" components={{ pink: <span className="fw-bold color-pink-diamond" /> }} /></h3>
-                    <a role="button" className="text-white" href='#me' onClick={e => scrollToElement(e)}>
-                        <FaAnglesDown className="text-white mt-4 fs-1 light-on" />
+                    <a role="button" href='#me' onClick={e => scrollToElement(e)}>
+                        <FaAnglesDown className="text-white mt-4 fs-1 light-on floating-animation" />
                     </a>
                 </div>
             </div>
